14c4121542fa80f9e3df9db7e99c88a43e2c9fb9
MichaelHinrichs <mchinrichs@gmail.com>
2024-03-30 13:09:05 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace Anarchy_Online_SDW_Extractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryReader br = new(File.OpenRead(args[0]));
+
+            int[] ends = new int[6];
+            for (int i = 0; i < 6; i++)
+                ends[i] = br.ReadInt32();
+
+            Directory.CreateDirectory(Path.GetDirectoryName(args[0]) + "//" + Path.GetFileNameWithoutExtension(args[0]));
+            int n = 0;
+            foreach (int end in ends)
+            {
+                BinaryWriter bw = new(File.Create(Path.GetDirectoryName(args[0]) + "//" + Path.GetFileNameWithoutExtension(args[0]) + "//" + n + ".png"));
+                bw.Write(br.ReadBytes(end - (int)br.BaseStream.Position));
+                bw.Close();
+                n++;
+            }
+        }
+    }
+}
